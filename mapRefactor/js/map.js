e5fe15ae700585e33c35ea5cf185f9a2be6c6036
Roman Brito <romanbrito1@gmail.com>
2017-09-01 19:52:31 -0500

    working on mapRefactor

---
+++ mapRefactor/js/map.js
@@ -0,0 +1,54 @@
+function initMap() {
+    // https://developers.google.com/maps/documentation/javascript/examples/distance-matrix
+    // https://developers.google.com/maps/documentation/javascript/reference
+
+    // create google map
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 30.395012, lng: -97.749017},
+        zoom: 10
+    });
+
+    // distance matrix
+    var service = new google.maps.DistanceMatrixService;
+    service.getDistanceMatrix({
+        origins: [{lat: 30.395012, lng: -97.749017}],
+        destinations: [{lat: 30.474465, lng: -97.801183}],
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.IMPERIAL,
+        avoidHighways: false,
+        avoidTolls: false
+    }, function (response, status) {
+        if (status === 'OK') {
+            console.log('distance matrix ' + JSON.stringify(response.rows[0].elements[0].distance.text));
+            console.log('distance matrix ' + JSON.stringify(response));
+        } else {
+            alert('Geocode was not successful due to: ' + status);
+        }
+    });
+
+    // markers
+    var markersArray = [];
+    markersArray.push(new google.maps.Marker({
+        map: map,
+        position: {lat: 30.395012, lng: -97.749017}
+    }));
+    markersArray.push(new google.maps.Marker({
+        map: map,
+        position: {"lat": 33.106217, "lng": -96.824965}
+    }));
+
+    // bounds
+    var bounds = new google.maps.LatLngBounds;
+    map.fitBounds(bounds.extend({"lat": 30.395012, "lng": -97.749017}));
+    map.fitBounds(bounds.extend({"lat": 30.474465, "lng": -97.801183}));
+    map.fitBounds(bounds.extend({"lat": 33.106217, "lng": -96.824965}));
+
+} // end function init
+
+// generate jsonp tag
+var tag = document.createElement("script");
+tag.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBZAdtCTX8ZlyU39tML3S_dOmmWWAh6cdk&callback=initMap';
+
+document.getElementsByTagName("body")[0].appendChild(tag);
+
+
