cfa67dbe1946fff3e08c642bcee62a115e08a811
Roman Brito <romanbrito1@gmail.com>
2017-09-02 22:05:29 -0500

    trying callback from hell

--- mapRefactor/js/map.js
+++ mapRefactor/js/map.js
@@ -1,35 +1,62 @@
-var EVT = new EventEmitter2(); // now can be used in multiple places,
-// doesn't need to be inside document.ready
+function initMap() { // outer function from jsonp
 
-function initMap() {
     // https://developers.google.com/maps/documentation/javascript/examples/distance-matrix
     // https://developers.google.com/maps/documentation/javascript/reference
+
+    var service = new google.maps.DistanceMatrixService;
+
+    // getting json object to have data available
+    $.getJSON('../json/locations.json', function (data) {
+
+        var locations = [];
+
+        getPosition(function (position) { //getPosition callback
+            var current_position = position;
+
+            $.each(data.locations, function (key, val) {
+
+                getDistance(current_position, data.locations[key].coordinates, service, function (distance) { // getDistance callback
+                    data.locations[key].distance = distance;
+                    locations.push(val);
+                }); // end getDistance callback
+
+            }) // end each
+
+
+        }); // end getPosition callback
+
+        console.log(locations);
+
+
+
+    }); // end get json object
+
+    // distance matrix
+
+    function getDistance(origin, destination, service, cb) {
+        service.getDistanceMatrix({
+            origins: [origin],
+            destinations: [destination],
+            travelMode: 'DRIVING',
+            unitSystem: google.maps.UnitSystem.IMPERIAL,
+            avoidHighways: false,
+            avoidTolls: false
+        }, function (response, status) {
+            if (status === 'OK') {
+                //console.log('distance matrix ' + JSON.stringify(response.rows[0].elements[0].distance.text));
+                cb(response.rows[0].elements[0].distance.text);
+                // console.log('distance matrix ' + JSON.stringify(response));
+            } else {
+                alert('Geocode was not successful due to: ' + status);
+            }
+        });
+    }
+
 
     // create google map
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 30.395012, lng: -97.749017},
         zoom: 10
-    });
-
-    // distance matrix
-    var service = new google.maps.DistanceMatrixService;
-
-    EVT.emit("google-service", service);
-
-    service.getDistanceMatrix({
-        origins: [{lat: 30.395012, lng: -97.749017}],
-        destinations: [{lat: 30.474465, lng: -97.801183}],
-        travelMode: 'DRIVING',
-        unitSystem: google.maps.UnitSystem.IMPERIAL,
-        avoidHighways: false,
-        avoidTolls: false
-    }, function (response, status) {
-        if (status === 'OK') {
-            // console.log('distance matrix ' + JSON.stringify(response.rows[0].elements[0].distance.text));
-            // console.log('distance matrix ' + JSON.stringify(response));
-        } else {
-            alert('Geocode was not successful due to: ' + status);
-        }
     });
 
     // markers
@@ -57,4 +84,26 @@ tag.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBZAdtCTX8ZlyU39tML3
 
 document.getElementsByTagName("body")[0].appendChild(tag);
 
+function getPosition(cb) {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
 
+            cb(pos); // callback function
+
+            //console.log('current position ' + JSON.stringify(pos));
+
+        }, function () {
+            //handleLocationError(true, infoWindow, map.getCenter());
+            console.log('error');
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        //handleLocationError(false, infoWindow, map.getCenter());
+        console.log('no geolocation');
+    }
+}
--- mapRefactor/js/searchLocation.js
+++ mapRefactor/js/searchLocation.js
@@ -1,132 +1,79 @@
-get_the_data(); // load info
-search_the_data(); // search data
-getDistance({lat: 30.395012, lng: -97.749017}, {lat: 30.474465, lng: -97.801183});
-getDistance({lat: 30.395012, lng: -97.749017}, {lat: 32.909483, lng: -96.959442});
-getPosition();
-console.log(locationsArray());
+var SearchLocation = (function () {
 
-function locationsArray () {
-    var locations = [];
-    $.getJSON('../json/locations.json', function (data) {
-        $.each(data.locations, function (key, val) {
-            locations.push(val);
-        })
-    });
+    function search_the_data(locations) {
+        $('#search').keyup(function () {
+            var searchField = $('#search').val();
+            var myExp = new RegExp(searchField, "i");
+            $.getJSON('../json/locations.json', function (data) {
+                var output = '<ul class="searchresults">';
+                $.each(data.locations, function (key, val) {
+                    if ((val.name.search(myExp) != -1) ||
+                        (val.address.search(myExp) != -1) ||
+                        (val.zip.search(myExp) != -1) ||
+                        (val.state.search(myExp) != -1) ||
+                        (val.city.search(myExp) != -1)) {
 
-    return locations;
-}
-
-function getPosition() {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            console.log('current position ' + JSON.stringify(pos));
-
-            return pos
-
-        }, function () {
-            //handleLocationError(true, infoWindow, map.getCenter());
-            console.log('error');
+                        output += get_output(val); // function
+                    }
+                });
+                output += '</ul>';
+                $('#update').html(output);
+            }); //get JSON
         });
-    } else {
-        // Browser doesn't support Geolocation
-        //handleLocationError(false, infoWindow, map.getCenter());
-        console.log('no geolocation');
     }
-}
 
-function getDistance(origin, destination) {
-    EVT.on("google-service", function (service) {
-        service.getDistanceMatrix({
-            origins: [origin],
-            destinations: [destination],
-            travelMode: 'DRIVING',
-            unitSystem: google.maps.UnitSystem.IMPERIAL,
-            avoidHighways: false,
-            avoidTolls: false
-        }, function (response, status) {
-            if (status === 'OK') {
-                console.log('distance matrix ' + JSON.stringify(response.rows[0].elements[0].distance.text));
-                // console.log('distance matrix ' + JSON.stringify(response));
-            } else {
-                alert('Geocode was not successful due to: ' + status);
-            }
-        });
-    })
-}
 
-function search_the_data() {
-    $('#search').keyup(function () {
-        var searchField = $('#search').val();
-        var myExp = new RegExp(searchField, "i");
+    function get_the_data() {
         $.getJSON('../json/locations.json', function (data) {
             var output = '<ul class="searchresults">';
             $.each(data.locations, function (key, val) {
-                if ((val.name.search(myExp) != -1) ||
-                    (val.address.search(myExp) != -1) ||
-                    (val.zip.search(myExp) != -1) ||
-                    (val.state.search(myExp) != -1) ||
-                    (val.city.search(myExp) != -1)) {
 
-                    output += get_output(val); // function
-                }
+                output += get_output(val); // function
+
+
             });
             output += '</ul>';
             $('#update').html(output);
-        }); //get JSON
-    });
-}
+        }); // get JSON
+    }
 
+    function get_output(val) {
+        var output = '';
 
-function get_the_data() {
-    $.getJSON('../json/locations.json', function (data) {
-        var output = '<ul class="searchresults">';
-        $.each(data.locations, function (key, val) {
+        output += '<li>';
+        output += '<div class="row main-location">';
 
-            output += get_output(val); // function
+        output += '<div class="location-info col-lg-6">';
+        output += '<h4>' + val.name + '</h4>';
+        output += '<p>' + val.address + '</p>';
+        output += '<p>' + val.city + ', ';
+        output += val.state + ' ';
+        output += val.zip + '</p>';
+        output += '<a href="tel:' + val.phone + '">' + 'T. ' + val.phone + '</a>';
+        output += '<p>' + val.hours1 + '</p>';
+        output += '<p>' + val.hours2 + '</p>';
+        output += '<p>' + val.hours3 + '</p>';
+        output += '</div>';
 
+        output += '<div class="location-buttons col-lg-6">';
+        output += '<div class="row">';
+        output += '<a class="btn btn-default col-lg-6 col-md-6 col-sm-6 col-xs-6" role="button" data-toggle="modal" data-target="' + '#' + val.label + 'menuModal"> Menu</a>';
+        output += '<a class="btn btn-default col-lg-6 col-md-6 col-sm-6 col-xs-6" role="button" data-toggle="modal" data-target="' + '#' + val.label + 'catModal"> Catering Menu</a>';
+        output += '</div>';
+        output += '<div class="row">';
+        output += '<a class="btn btn-danger" href="https://www.google.com/maps/dir/?api=1&destination=' + val.coordinates.lat + ',' + val.coordinates.lng + '" target="_blank" role="button"> Directions</a>';
+        output += '</div>';
+        output += '</div>';
 
-        });
-        output += '</ul>';
-        $('#update').html(output);
-    }); // get JSON
-}
+        output += '</div>';
+        output += '</li>';
 
-function get_output(val) {
-    var output = '';
+        return output;
+    }
 
-    output += '<li>';
-    output += '<div class="row main-location">';
+    return {
+        getData: get_the_data,
+        searchData: search_the_data
+    }
 
-    output += '<div class="location-info col-lg-6">';
-    output += '<h4>' + val.name + '</h4>';
-    output += '<p>' + val.address + '</p>';
-    output += '<p>' + val.city + ', ';
-    output += val.state + ' ';
-    output += val.zip + '</p>';
-    output += '<a href="tel:' + val.phone + '">' + 'T. ' + val.phone + '</a>';
-    output += '<p>' + val.hours1 + '</p>';
-    output += '<p>' + val.hours2 + '</p>';
-    output += '<p>' + val.hours3 + '</p>';
-    output += '</div>';
-
-    output += '<div class="location-buttons col-lg-6">';
-    output += '<div class="row">';
-    output += '<a class="btn btn-default col-lg-6 col-md-6 col-sm-6 col-xs-6" role="button" data-toggle="modal" data-target="' + '#' + val.label + 'menuModal"> Menu</a>';
-    output += '<a class="btn btn-default col-lg-6 col-md-6 col-sm-6 col-xs-6" role="button" data-toggle="modal" data-target="' + '#' + val.label + 'catModal"> Catering Menu</a>';
-    output += '</div>';
-    output += '<div class="row">';
-    output += '<a class="btn btn-danger" href="https://www.google.com/maps/dir/?api=1&destination=' + val.coordinates.lat + ',' + val.coordinates.lng + '" target="_blank" role="button"> Directions</a>';
-    output += '</div>';
-    output += '</div>';
-
-    output += '</div>';
-    output += '</li>';
-
-    return output;
-}
+})();
