2471ed0af1cf4b894a3fbbc5e09e375c927c87ab
Roman Brito <romanbrito1@gmail.com>
2017-10-21 17:07:51 -0500

    maps more modular

--- locationsTexadelphia/callMaps.js
+++ locationsTexadelphia/callMaps.js
@@ -1,13 +1,23 @@
 function loadMapsScript() {
-    var script = document.createElement('script');
+    var Google_Maps_API_Script = document.createElement('script');
 
-    script.onload = function () {
-        var o = GoogleMap();
-        o.addMarkers(o.getGoogleMaps().map);
+    Google_Maps_API_Script.onload = function () {
+
+        $.getJSON('locations.json', function (data) {
+
+            var o = GoogleMap();
+        o.addMarkers(o.getGoogleMaps( data ).map, data);
+
+        });
+
     };
-    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBZAdtCTX8ZlyU39tML3S_dOmmWWAh6cdk";
 
-    document.head.appendChild(script);
+    Google_Maps_API_Script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBZAdtCTX8ZlyU39tML3S_dOmmWWAh6cdk";
+
+    document.head.appendChild(Google_Maps_API_Script);
 }
 
 loadMapsScript();
+
+
+
