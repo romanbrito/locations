fe868c32945f992cf29d8a3e183cfe4fa1612e0f
Roman Brito <romanbrito1@gmail.com>
2017-10-21 00:10:38 -0500

    added locationsTexadelphia

---
+++ locationsTexadelphia/googleMaps.js
@@ -0,0 +1,45 @@
+function loadMapsScript() {
+    var script = document.createElement('script');
+
+    script.onload = function () {
+        var o = GoogleMap();
+        o.addMarkers(o.getGoogleMaps().map);
+    };
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBZAdtCTX8ZlyU39tML3S_dOmmWWAh6cdk";
+
+    document.head.appendChild(script);
+}
+
+function GoogleMap() {
+
+        function getGoogleMaps() {
+
+            var position = {lat: 28.3852377, lng: -81.5660627};
+            var google_map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 10,
+                center: position
+            });
+
+            return {
+                map: google_map
+            }
+
+        }
+
+        function addMarkers( map ) {
+
+                var position = {lat: 28.3852377, lng: -81.5660627};
+
+                var marker = new google.maps.Marker({
+                    position: position,
+                    map: map
+                });
+
+        }
+
+        return {
+            getGoogleMaps: getGoogleMaps,
+            addMarkers: addMarkers
+        };
+}
+
