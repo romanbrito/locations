129bb94142a1b055553a982a5d53f7d92e8f6fae
Roman Brito <romanbrito1@gmail.com>
2017-10-21 00:12:19 -0500

    starting module orgainzation to locations texadelphia

--- locationsTexadelphia/callMaps.js
+++ locationsTexadelphia/callMaps.js
@@ -1,1 +1,13 @@
+function loadMapsScript() {
+    var script = document.createElement('script');
+
+    script.onload = function () {
+        var o = GoogleMap();
+        o.addMarkers(o.getGoogleMaps().map);
+    };
+    script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBZAdtCTX8ZlyU39tML3S_dOmmWWAh6cdk";
+
+    document.head.appendChild(script);
+}
+
 loadMapsScript();
